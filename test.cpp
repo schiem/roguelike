66b335f506a7da3af9c848369dc72ce55dc3d85b
Seth Yoder <seth.a.yoder@gmail.com>
2013-03-17 22:45:33 -0400

    Started work on path-digging

---
+++ test.cpp
@@ -0,0 +1,14 @@
+
+#include <DungeonBuilder.h>
+#include <Character.h>
+#include <ncurses.h>
+#include <stdio.h>
+
+
+int main()
+{
+	DungeonBuilder dungeon(50, 40);
+    dungeon.build_pblind_dungeon(1, 1, 1);
+   
+	return 0;	
+}
