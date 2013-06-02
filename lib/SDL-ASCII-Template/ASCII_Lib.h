866b052c3324e9d9a32f4ce8403afc565cf55f1a
Seth Yoder <seth.a.yoder@gmail.com>
2013-06-01 23:50:37 -0400

    Did away with the submodule and just made it part of our project. Michael, if you don't like that, we can change it back, but SDL-ASCII-Template as it is is just incompatible with things. For example, it uses itoa() which is deprecated, and it includes SDL.h instead of SDL/SDL.h...

---
+++ lib/SDL-ASCII-Template/ASCII_Lib.h
@@ -0,0 +1,17 @@
+#ifndef ASCII_LIB_H
+#define ASCII_LIB_H
+#include <SDL/SDL.h>
+#include <def.h>
+#include <math.h>
+
+
+// Print an ASCII character
+void drawChr(int x, int y, int smiley, SDL_Surface* source, SDL_Surface* destination, Uint32 color);
+
+// Print a string
+void drawStr(int x, int y, char stringy[], SDL_Surface* source, SDL_Surface* destination, Uint32 color);
+
+// Print a number
+void drawInt(int x, int y, int number, SDL_Surface* source, SDL_Surface* destination, Uint32 color, int size = 0);
+
+#endif
