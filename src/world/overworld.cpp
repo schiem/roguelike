dc15cc135c56894f05586a14e8518c97637c491a
Seth Yoder <seth.a.yoder@gmail.com>
2013-12-21 01:00:09 -0500

    In a frenzy of rage I made trees generate much more sparsely. Setting them back to where they were before.

--- src/world/overworld.cpp
+++ src/world/overworld.cpp
@@ -28,7 +28,7 @@ Overworld::Overworld(int _width, int _height, bool _is_dungeon) {
     ground = TileMatrix(height, std::vector<Tile>(width, EMPTY));
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
-            if (rand() % 18 == 0){
+            if (rand() % 10 == 0){
                 ground[i][j] = TREE;
             } else {
                 ground[i][j] = OVERWORLD_DIRT;
