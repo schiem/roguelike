13e139a2cc624de58ff0368e11afbece1d858c02
Seth Yoder <seth.a.yoder@gmail.com>
2013-12-23 01:56:40 -0500

    Modified build to include boost_system and boost_filesystem libs. These will be necessary for serializing data. I hope to work at least a little bit of that out this week.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "gui.h"
+#include <boost/filesystem.hpp>
+
 using namespace std;
 
+namespace fs=boost::filesystem;
+
 int main(int argc, char* args[]) {
+
+    cout<<"Testing boost with /etc/fstab file size: "<<fs::file_size("/etc/fstab")<<endl;
+
     GUI theApp;
     return theApp.OnExecute();
 }
