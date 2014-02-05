6dde62676b0f6fce760a145319124a1e0e0e5b62
schiem <mjyoder@goshen.edu>
2014-02-05 15:35:18 -0500

    Added a snarky comment ;)

--- src/gui/game.cpp
+++ src/gui/game.cpp
@@ -96,6 +96,7 @@ void Game::init(const MapTileMatrix& _world_map, IntPoint selected_chunk) {
 
     //TODO THIS IS A TEMPORARY HACK THAT SHOULD BE GONE BEFORE I GO TO TANZANIA,
     //SO HELP ME GOD.
+    //lol -m, 2/5/2014
     block_wall_tile = BLOCK_WALL;
 
     initialized = true;
