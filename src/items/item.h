bf49bd916b3cdf2b948dc6762526c6238bad5014
Schiem <schiem01@gmail.com>
2014-02-12 21:53:19 -0600

    Started adding items.

---
+++ src/items/item.h
@@ -0,0 +1,38 @@
+/**
+ *  ITEM.H
+ *
+ *  This file is part of ROGUELIKETHING.
+ *
+ *  ROGUELIKETHING is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ROGUELIKETHING is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ROGUELIKETHING.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ITEM_H
+#define ITEM_H
+#include <terrain_defs.h>
+
+class Item
+{
+    protected:
+        int weight;
+        Tile sprite;
+
+
+    public:
+        Item(int, Tile);
+        int get_weight();
+        Tile get_sprite();
+
+};
+
+#endif
